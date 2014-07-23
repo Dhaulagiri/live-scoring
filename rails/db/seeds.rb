597664feb322c727e9e3def5aff72d5a44a1ccb4
Brian Runnells <brian.runnells@gmail.com>
2014-07-22 20:45:38 -0600

    Initial seeds file

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts 'DEFAULT USERS'
+user_list = [
+	['Brian Runnells', 'brian.runnells@gmail.com', 'climb514', 'admin'],
+]
+user_list.each do |name, email, password, role|
+  user = User.find_or_create_by_name name: name, email: email, password: password, password_confirmation: password
+  puts 'user: ' << user.name
+  user.add_role role
+end
+
+puts 'Disciplines'
+disciplines = ['Bouldering', 'Sport Climbing', 'Speed Climbing']
+disciplines.each do |name|
+  discipline = Discipline.create(name: name)
+end
