a3641a7329ae22a5a43fd45b3d2c0399e82f1124
Brian Runnells <brian.runnells@gmail.com>
2014-08-31 17:28:15 -0600

    Unit test for climber fullName computed property

--- ember/tests/unit/models/climber-test.js
+++ ember/tests/unit/models/climber-test.js
@@ -10,3 +10,14 @@ test('it exists', function() {
   // var store = this.store();
   ok(model);
 });
+
+test('fullName', function() {
+  var climber = this.subject({ firstName: "Chris", lastName: "Sharma"});
+  equal(climber.get('fullName'), "Chris Sharma");
+
+  Ember.run(function(){
+    climber.set('middleName','Freaking');
+  });
+
+  equal(climber.get('fullName'), "Chris Freaking Sharma");
+});
