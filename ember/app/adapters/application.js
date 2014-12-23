222d4c005c1e35b6b2d0b697f65a45a4d79f0c48
Brian Runnells <brian.runnells@gmail.com>
2014-12-23 16:06:53 -0700

    Use different adapter for test

--- ember/app/adapters/application.js
+++ ember/app/adapters/application.js
@@ -1,6 +1,16 @@
 import DS from 'ember-data';
 import config from '../config/environment';
 
-export default DS.FirebaseAdapter.extend({
-  firebase: new window.Firebase('https://' + config.firebase_instance + '.firebaseio.com')
-});
+var ApplicationAdapter;
+
+if (config.environment === "test") {
+  ApplicationAdapter = DS.ActiveModelAdapter.extend({
+    namespace: 'api/v1'
+  });
+} else {
+  ApplicationAdapter = DS.FirebaseAdapter.extend({
+    firebase: new window.Firebase('https://' + config.firebase_instance + '.firebaseio.com')
+  });
+}
+
+export default ApplicationAdapter;
